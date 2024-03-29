dcdfac1665803f8077c5014c20807eb75a660af1
Pillow.K <pillowk@feelanet.com>
2024-03-29 19:10:38 +0900

    Commit

---
+++ GraphSample/TokenProvider.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.Kiota.Abstractions.Authentication;
+
+namespace GraphSample
+{
+    public class TokenProvider : IAccessTokenProvider
+    {
+        private readonly string _userAssert;
+        public IConfidentialClientApplication Application { get; set; }
+        public AllowedHostsValidator AllowedHostsValidator { get; }
+
+        public TokenProvider(string clientId, string tenentId, string clientSecret, string userAssert)
+        {
+            this.AllowedHostsValidator = new AllowedHostsValidator();
+            this.Application = ConfidentialClientApplicationBuilder
+                .Create(clientId)
+                .WithTenantId(tenentId)
+                .WithClientSecret(clientSecret)
+                .Build();
+
+            _userAssert = userAssert;
+        }
+
+        public async Task<string> GetAuthorizationTokenAsync(
+            Uri uri,
+            Dictionary<string, object> additionalAuthenticationContext = null,
+            CancellationToken cancellationToken = default)
+        {
+            List<string> scopeURL = new List<string>();
+            scopeURL.Add("https://graph.microsoft.com/.default");
+
+            var accessToken = await this.Application.AcquireTokenOnBehalfOf(
+                scopeURL,
+                new UserAssertion())
+                .ExecuteAsync();
+
+            return accessToken.AccessToken;
+        }
+    }
+}
